4c8d673c3783d5a23e78e4f0daf5ffb91fcc64b6
JuliaL131313 <76820718+JuliaL131313@users.noreply.github.com>
2021-12-31 19:41:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+const imgBlock = document.querySelector('.img-block');
+const btn = document.querySelector('.btn');
+const box = document.querySelector('.box');
+const happyText = document.querySelector('.happy-text');
+
+function showCongrat() {
+
+  imgBlock.style.display = 'block';
+  imgBlock.style.width = '80%';
+  imgBlock.style.maxWidth='500px';
+  imgBlock.style.margin = '15px auto 0';
+
+  happyText.style.display = 'block';
+
+  box.style.display = 'none';
+  showCongratText();
+}
+btn.addEventListener('click', showCongrat);
+
+function showCongratText() {
+class TextScramble {
+   constructor(el) {
+      this.el = el;
+      this.chars = '!<>-_\\/[]{}—=+*^?#________';
+      this.update = this.update.bind(this);
+   }
+   setText(newText) {
+      const oldText = this.el.innerText;
+      const length = Math.max(oldText.length, newText.length);
+      const promise = new Promise((resolve) => this.resolve = resolve);
+      this.queue = [];
+      for (let i = 0; i < length; i++) {
+         const from = oldText[i] || '';
+         const to = newText[i] || '';
+         const start = Math.floor(Math.random() * 40);
+         const end = start + Math.floor(Math.random() * 40);
+         this.queue.push({ from, to, start, end });
+      }
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+   }
+   update() {
+      let output = '';
+      let complete = 0;
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+         let { from, to, start, end, char } = this.queue[i];
+         if (this.frame >= end) {
+            complete++;
+            output += to;
+         } else if (this.frame >= start) {
+            if (!char || Math.random() < 0.28) {
+               char = this.randomChar();
+               this.queue[i].char = char;
+            }
+            output += `<span class="dud">${char}</span>`;
+         } else {
+            output += from;
+         }
+      }
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+         this.resolve();
+      } else {
+         this.frameRequest = requestAnimationFrame(this.update);
+         this.frame++;
+      }
+   }
+   randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
+   }
+ }
+ 
+ let phrases = ['Не умрите!', 'Не спейтесь!',
+   'Не потолстейте!', "Не теряйте надежду и деньги!", "Happy New Year i КРЕПИТЕСЬ!", 'end'];
+ 
+ const el = document.querySelector('.happy-text');
+ const fx = new TextScramble(el);
+ 
+ let counter = 0;
+ const next = () => {
+   fx.setText(phrases[counter]).then(() => {
+      let timer = setTimeout(next, 600);
+      if (counter === phrases.length - 1) {
+         clearTimeout(timer);
+      }
+   })
+   counter = (counter + 1) % phrases.length;
+ }
+ 
+ next();
+}
